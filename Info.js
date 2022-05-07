b7144e76455ecc63274d0a7e6a21ae1d0fd9f12e
Gri12388 <gri12388@yandex.ru>
2022-05-07 13:15:08 +0400

    feat: add Info.js

---
+++ Info.js
@@ -0,0 +1,5 @@
+export default class Info {
+  os = ['The keybourd is made for Windows OS', 'Клавиатура создана для операционной системы Windows'];
+
+  switcher = ['To switch language press Alt + Shift', 'Для смены языка нажмите Alt + Shift'];
+}
